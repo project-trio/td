984d576895ef4058f26834d791ceffab0caf2d05
Kyle Coburn <kylecoburn@gmail.com>
2018-06-25 21:20:36 -0700

    ♻️ Paths class

--- src/play/Game/Map.js
+++ src/play/Game/Map.js
@@ -6,28 +6,22 @@ import render from '@/play/render'
 
 import Creep from '@/play/Game/entity/Creep'
 import Tower from '@/play/Game/entity/Tower'
+import Paths from '@/play/Game/Paths'
 
 const TILE_SIZE = 32
 const TILES_WIDE = 22
 const TILES_TALL = 18
 const MAP_WIDTH = TILE_SIZE * TILES_WIDE
 const MAP_HEIGHT = TILE_SIZE * TILES_TALL
-const TOTAL_TILES = TILES_WIDE * TILES_TALL
 
 const MIN_X = -MAP_WIDTH / 2 + TILE_SIZE * 2
 const MIN_Y = -MAP_HEIGHT / 2 + TILE_SIZE * 2
 const MAX_X = MAP_WIDTH / 2 - TILE_SIZE * 2
 const MAX_Y = MAP_HEIGHT / 2 - TILE_SIZE * 2
 
-const BLOCK_CHECK = [0, -1, -TILES_WIDE, 1]
-
 const ENTRANCE_SIZE = 6
 const EX = 0
 const EY = 1
-
-const tileArray = () => {
-	return new Array(TOTAL_TILES)
-}
 
 export default class GameMap {
 
@@ -41,32 +35,7 @@ export default class GameMap {
 		this.container = render.group(parent)
 		this.container.interactive = true
 
-		this.blocked = tileArray()
-		this.test = [ tileArray(), tileArray() ]
-		this.entrances = [ this.entrance(true, false), this.entrance(true, true) ]
-		this.exits = [ this.entrance(false, false), this.entrance(false, true) ]
-		this.ordinals = [
-			[ [1, 0], [0, -TILES_WIDE], [-1, 0], [0, TILES_WIDE] ],
-			[ [1, -TILES_WIDE], [-1, -TILES_WIDE], [-1, TILES_WIDE], [1, TILES_WIDE] ],
-		]
-
-		let blockCol = 1, blockRow = 1
-		for (let idx = 0; idx < TOTAL_TILES; idx += 1) {
-			let isBlocked = false
-			if (blockCol === 1 || blockCol === TILES_WIDE) {
-				isBlocked = blockRow <= TTH - EH + EY || blockRow > TTH + EH + EY
-			} else if (blockRow === 1 || blockRow === TILES_TALL) {
-				isBlocked = blockCol <= TWH - EH + EX || blockCol > TWH + EH + EX
-			}
-			this.blocked[idx] = isBlocked
-			if (blockCol < TILES_WIDE) {
-				blockCol += 1
-			} else {
-				blockRow += 1
-				blockCol = 1
-			}
-		}
-		this.updatePaths(false)
+		this.paths = new Paths(TILES_WIDE, TILES_TALL, ENTRANCE_SIZE, EX, EY)
 
 		const ground = render.rectangle(MAP_WIDTH, MAP_HEIGHT, { color: 0xccbb99, parent: this.container })
 		ground.owner = ground
@@ -118,9 +87,9 @@ export default class GameMap {
 			}
 			if (tx !== cx || ty !== cy) {
 				if (cx !== null) {
-					this.toggleTower(cx, cy, false)
+					this.paths.toggleTower(cx, cy, false)
 				}
-				if (this.blockedSquare(tx, ty)) {
+				if (this.paths.blockedSquare(tx, ty)) {
 					cx = null
 					placement.visible = false
 					return
@@ -130,9 +99,9 @@ export default class GameMap {
 				cy = ty
 				placement.position.x = tx * TILE_SIZE - MAP_WIDTH / 2
 				placement.position.y = ty * TILE_SIZE - MAP_HEIGHT / 2
-				this.toggleTower(cx, cy, true)
+				this.paths.toggleTower(cx, cy, true)
 
-				const blocked = !this.updatePaths(true)
+				const blocked = !this.paths.update(true)
 				if (blocked !== placement.blocked) {
 					placement.blocked = blocked
 					placement.material.color.setHex(blocked ? 0xdd8855 : 0x99dd66)
@@ -150,8 +119,8 @@ export default class GameMap {
 			}
 			placement.visible = false
 			const tower = new Tower(placement.position.x, placement.position.y, this.container)
-			this.toggleTower(cx, cy, true, false)
-			this.updatePaths(false)
+			this.paths.toggleTower(cx, cy, true, false)
+			this.paths.update(false)
 			tower.tx = cx
 			tower.ty = cy
 			cx = null
@@ -159,152 +128,9 @@ export default class GameMap {
 		}
 	}
 
-	toggleTower (cx, cy, blocking) {
-		let index = cx + (TILES_TALL - cy) * TILES_WIDE
-		for (const diff of BLOCK_CHECK) {
-			index += diff
-			this.blocked[index] = blocking
-		}
-	}
-
-	blockedSquare (cx, cy) {
-		let index = cx + (TILES_TALL - cy) * TILES_WIDE
-		for (const diff of BLOCK_CHECK) {
-			index += diff
-			if (this.blocked[index]) {
-				return true
-			}
-		}
-		return false
-	}
-
-	entrance (enter, vertical) {
-		const result = []
-		let tx = vertical ? (TILES_WIDE - ENTRANCE_SIZE) / 2 : (enter ? 1 : TILES_WIDE)
-		let ty = vertical ? (enter ? 1 : TILES_TALL) : (TILES_TALL - ENTRANCE_SIZE) / 2
-		if (vertical) {
-			tx += EX
-		} else {
-			ty += EY
-		}
-		for (let eidx = 0; eidx < ENTRANCE_SIZE; eidx += 1) {
-			if (vertical) {
-				tx += 1
-			} else {
-				ty += 1
-			}
-			result[eidx] = (tx - 1) + (ty - 1) * TILES_WIDE
-		}
-		return result
-	}
-
 	spawn () {
 		store.state.game.wave += 1
 		console.log('Wave', store.state.game.wave)
-	}
-
-	//PATHFINDING
-
-	updatePaths (test) {
-		if (!test) {
-			this.paths = [ tileArray(), tileArray() ]
-		}
-		return this.pathfind(test, true) && this.pathfind(test, false)
-	}
-
-	pathfind (test, vertical) {
-		//TODO if test === false then include creep occupied squares
-		// console.time('path ' + vertical)
-		const path = (test ? this.test : this.paths)[vertical ? 1 : 0]
-		const requiredIndecies = new Set(this.entrances[vertical ? 1 : 0])
-		let positions = [ ...this.exits[vertical ? 1 : 0] ]
-		const searchedIndexes = new Set()
-		const blocked = this.blocked
-		for (let idx = 0; idx < TOTAL_TILES; idx += 1) {
-			if (blocked[idx]) {
-				searchedIndexes.add(idx)
-			}
-		}
-		for (const position of positions) {
-			searchedIndexes.add(position)
-		}
-
-		let firstSearch = true
-		let foundPath = false
-		while (positions.length) {
-			const nextSearch = []
-			let diagonal = false
-			for (const indexDiffs of this.ordinals) {
-				for (const position of positions) {
-					const column = position % TILES_WIDE
-					const edgeCheck = column === 0 ? -1 : (column === TILES_WIDE - 1 ? 1 : false)
-					for (const diffs of indexDiffs) {
-						const diffX = diffs[0], diffY = diffs[1]
-						if (edgeCheck && diffX === edgeCheck) {
-							continue
-						}
-						const change = diffX + diffY
-						const newIndex = position + change
-						if (newIndex <= 0 || newIndex >= TOTAL_TILES || searchedIndexes.has(newIndex)) {
-							continue
-						}
-						if (diagonal && (blocked[position + diffX] || blocked[position + diffY])) {
-							continue
-						}
-						if (requiredIndecies.delete(newIndex) && !requiredIndecies.size) {
-							foundPath = true
-						}
-						path[newIndex] = [ Math.sign(-diffX), Math.sign(-diffY) ]
-						nextSearch.push(newIndex)
-						searchedIndexes.add(newIndex)
-					}
-				}
-				if (firstSearch) {
-					break
-				}
-				diagonal = !diagonal
-			}
-			if (!nextSearch.length) {
-				break
-			}
-			firstSearch = false
-			positions = nextSearch
-		}
-		// console.timeEnd('path ' + vertical)
-		if (!test) { //SAMPLE
-			if (!foundPath) {
-				console.error('Unable to calculate required path', vertical, requiredIndecies, blocked)
-				this.debugPath(path, false)
-				return true
-			}
-			// this.debugPath(path, foundPath && entranceTest) //SAMPLE
-		}
-		return foundPath
-	}
-
-	debugPath (path, entrance) {
-		const ARROWS = {
-			'10': '→',
-			'1-1': '↗',
-			'0-1': '↑',
-			'-1-1': '↖',
-			'-10': '←',
-			'-11': '↙',
-			'01': '↓',
-			'11': '↘',
-		}
-		let count = 0, out = '%c'
-		for (let idx = 0; idx < TOTAL_TILES; idx += 1) {
-			const pi = path[idx]
-			out += (pi ? ARROWS[`${pi[0]}${pi[1]}`] : '·') + ' '
-			if (count >= TILES_WIDE - 1) {
-				count = 0
-				out += '\n'
-			} else {
-				count += 1
-			}
-		}
-		console.log(out, `color:${entrance ? 'black' : 'red'};`)
 	}
 
 }
---
+++ src/play/Game/Paths.js
@@ -0,0 +1,194 @@
+let TILES_WIDE, TILES_TALL, TILES_TOTAL
+let ENTRANCE_SIZE, EX, EY
+
+const tileArray = () => {
+	return new Array(TILES_TOTAL)
+}
+
+const entrance = (enter, vertical) => {
+	const result = []
+	let tx = vertical ? (TILES_WIDE - ENTRANCE_SIZE) / 2 : (enter ? 1 : TILES_WIDE)
+	let ty = vertical ? (enter ? 1 : TILES_TALL) : (TILES_TALL - ENTRANCE_SIZE) / 2
+	if (vertical) {
+		tx += EX
+	} else {
+		ty += EY
+	}
+	for (let eidx = 0; eidx < ENTRANCE_SIZE; eidx += 1) {
+		if (vertical) {
+			tx += 1
+		} else {
+			ty += 1
+		}
+		result[eidx] = (tx - 1) + (ty - 1) * TILES_WIDE
+	}
+	return result
+}
+
+export default class Paths {
+
+	constructor (tilesWide, tilesTall, entranceSize, ex, ey) {
+		TILES_WIDE = tilesWide
+		TILES_TALL = tilesTall
+		ENTRANCE_SIZE = entranceSize
+		TILES_TOTAL = TILES_WIDE * TILES_TALL
+		EX = ex
+		EY = ey
+		this.blockCheck = [0, -1, -TILES_WIDE, 1]
+
+		this.blocked = tileArray()
+		this.test = [ tileArray(), tileArray() ]
+		this.entrances = [ entrance(true, false), entrance(true, true) ]
+		this.exits = [ entrance(false, false), entrance(false, true) ]
+		this.ordinals = [
+			[ [1, 0], [0, -TILES_WIDE], [-1, 0], [0, TILES_WIDE] ],
+			[ [1, -TILES_WIDE], [-1, -TILES_WIDE], [-1, TILES_WIDE], [1, TILES_WIDE] ],
+		]
+
+		let blockCol = 1, blockRow = 1
+		for (let idx = 0; idx < TILES_TOTAL; idx += 1) {
+			let isBlocked = false
+			if (blockCol === 1 || blockCol === TILES_WIDE) {
+				const entranceStart = (TILES_TALL - entranceSize) / 2 + ey
+				if (blockRow <= entranceStart || blockRow > entranceStart + entranceSize) {
+					isBlocked = true
+				}
+			} else if (blockRow === 1 || blockRow === TILES_TALL) {
+				const entranceStart = (TILES_WIDE - entranceSize) / 2 + ex
+				if (blockCol <= entranceStart || blockCol > entranceStart + entranceSize) {
+					isBlocked = true
+				}
+			}
+			this.blocked[idx] = isBlocked
+			if (blockCol < TILES_WIDE) {
+				blockCol += 1
+			} else {
+				blockRow += 1
+				blockCol = 1
+			}
+		}
+		this.update(false)
+	}
+
+	toggleTower (cx, cy, blocking) {
+		let index = cx + (TILES_TALL - cy) * TILES_WIDE
+		for (const diff of this.blockCheck) {
+			index += diff
+			this.blocked[index] = blocking
+		}
+	}
+
+	blockedSquare (cx, cy) {
+		let index = cx + (TILES_TALL - cy) * TILES_WIDE
+		for (const diff of this.blockCheck) {
+			index += diff
+			if (this.blocked[index]) {
+				return true
+			}
+		}
+		return false
+	}
+
+	update (test) {
+		if (!test) {
+			this.paths = [ tileArray(), tileArray() ]
+		}
+		return this.pathfind(test, true) && this.pathfind(test, false)
+	}
+
+	pathfind (test, vertical) {
+		//TODO if test === false then include creep occupied squares
+		// console.time('path ' + vertical)
+		const path = (test ? this.test : this.paths)[vertical ? 1 : 0]
+		const requiredIndecies = new Set(this.entrances[vertical ? 1 : 0])
+		let positions = [ ...this.exits[vertical ? 1 : 0] ]
+		const searchedIndexes = new Set()
+		const blocked = this.blocked
+		for (let idx = 0; idx < TILES_TOTAL; idx += 1) {
+			if (blocked[idx]) {
+				searchedIndexes.add(idx)
+			}
+		}
+		for (const position of positions) {
+			searchedIndexes.add(position)
+		}
+
+		let firstSearch = true
+		let foundPath = false
+		while (positions.length) {
+			const nextSearch = []
+			let diagonal = false
+			for (const indexDiffs of this.ordinals) {
+				for (const position of positions) {
+					const column = position % TILES_WIDE
+					const edgeCheck = column === 0 ? -1 : (column === TILES_WIDE - 1 ? 1 : false)
+					for (const diffs of indexDiffs) {
+						const diffX = diffs[0], diffY = diffs[1]
+						if (edgeCheck && diffX === edgeCheck) {
+							continue
+						}
+						const change = diffX + diffY
+						const newIndex = position + change
+						if (newIndex <= 0 || newIndex >= TILES_TOTAL || searchedIndexes.has(newIndex)) {
+							continue
+						}
+						if (diagonal && (blocked[position + diffX] || blocked[position + diffY])) {
+							continue
+						}
+						if (requiredIndecies.delete(newIndex) && !requiredIndecies.size) {
+							foundPath = true
+						}
+						path[newIndex] = [ Math.sign(-diffX), Math.sign(-diffY) ]
+						nextSearch.push(newIndex)
+						searchedIndexes.add(newIndex)
+					}
+				}
+				if (firstSearch) {
+					break
+				}
+				diagonal = !diagonal
+			}
+			if (!nextSearch.length) {
+				break
+			}
+			firstSearch = false
+			positions = nextSearch
+		}
+		// console.timeEnd('path ' + vertical)
+		if (!test) { //SAMPLE
+			if (!foundPath) {
+				console.error('Unable to calculate required path', vertical, requiredIndecies, blocked)
+				this.debugPath(path, false)
+				return true
+			}
+			// this.debugPath(path, foundPath && entranceTest) //SAMPLE
+		}
+		return foundPath
+	}
+
+	debugPath (path, entrance) {
+		const ARROWS = {
+			'10': '→',
+			'1-1': '↗',
+			'0-1': '↑',
+			'-1-1': '↖',
+			'-10': '←',
+			'-11': '↙',
+			'01': '↓',
+			'11': '↘',
+		}
+		let count = 0, out = '%c'
+		for (let idx = 0; idx < TILES_TOTAL; idx += 1) {
+			const pi = path[idx]
+			out += (pi ? ARROWS[`${pi[0]}${pi[1]}`] : '·') + ' '
+			if (count >= TILES_WIDE - 1) {
+				count = 0
+				out += '\n'
+			} else {
+				count += 1
+			}
+		}
+		console.log(out, `color:${entrance ? 'black' : 'red'};`)
+	}
+
+}
