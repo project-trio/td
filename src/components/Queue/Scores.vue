2ba7c8e82e5d57043e9f9983c30a0ca6bd3c5183
Kyle Coburn <kylecoburn@gmail.com>
2018-07-09 18:26:16 -0700

    💄 Show highscore rank

--- src/components/Queue/Scores.vue
+++ src/components/Queue/Scores.vue
@@ -2,9 +2,9 @@
 <div>
 	<h2>Highscores</h2>
 	<table>
-		<tr><th>User</th><th>Score</th></tr>
-		<tr v-for="score in scores" :class="{ highlight: score[0] === localId }" :key="score[0]">
-			<td><User :id="score[0]" :size="32" /></td><td><Time :duration="score[1] * 1000" /></td>
+		<tr><th>Rank</th><th>User</th><th>Score</th></tr>
+		<tr v-for="(score, idx) in scores" :class="{ highlight: score[0] === localId }" :key="score[0]">
+			<td>{{ idx + 1 }}</td><td><User :id="score[0]" :size="32" /></td><td><Time :duration="score[1] * 1000" /></td>
 		</tr>
 	</table>
 </div>
@@ -40,10 +40,10 @@ export default {
 table
 	margin auto
 	border-collapse collapse
+	min-width 288px
 
 th, td
 	padding 2px 8px
-	min-width 96px
 	box-sizing border-box
 
 .highlight
